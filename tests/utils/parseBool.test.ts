4e7506cdf76a06de39a962f7660ef270aca41b31
Garrett Downs <garredow@outlook.com>
2021-09-12 17:53:43 -0400

    fix: trigger deployment

--- tests/utils/parseBool.test.ts
+++ tests/utils/parseBool.test.ts
@@ -4,6 +4,8 @@ describe('parseBool()', () => {
   const cases = [
     { source: 'true', expectedVal: true, defaultVal: undefined },
     { source: 'TRUE', expectedVal: true, defaultVal: undefined },
+    { source: 'false', expectedVal: false, defaultVal: undefined },
+    { source: 'FALSE', expectedVal: false, defaultVal: undefined },
   ];
   it.each(cases)(
     'should return value',
