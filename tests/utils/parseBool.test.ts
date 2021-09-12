77e554fbb9b2ab6b093fbcec7e3d93be218eb321
Garrett Downs <garredow@outlook.com>
2021-09-12 18:06:04 -0400

    test(parsebool): add test cases

--- tests/utils/parseBool.test.ts
+++ tests/utils/parseBool.test.ts
@@ -6,6 +6,10 @@ describe('parseBool()', () => {
     { source: 'TRUE', expectedVal: true, defaultVal: undefined },
     { source: 'false', expectedVal: false, defaultVal: undefined },
     { source: 'FALSE', expectedVal: false, defaultVal: undefined },
+    { source: undefined, expectedVal: true, defaultVal: true },
+    { source: undefined, expectedVal: false, defaultVal: false },
+    { source: 'other', expectedVal: true, defaultVal: true },
+    { source: 'OTHER', expectedVal: false, defaultVal: false },
   ];
   it.each(cases)(
     'should return value',
