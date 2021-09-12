fa8b26f9c52f375238c5449a350c63d007e677c0
Garrett Downs <garredow@outlook.com>
2021-09-12 12:53:37 -0400

    feat(release.config.js): change commit options

--- release.config.js
+++ release.config.js
@@ -17,13 +17,6 @@ module.exports = {
         failComment: false,
       },
     ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md'],
-        message:
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    '@semantic-release/git',
   ],
 };
